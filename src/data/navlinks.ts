c6d08e158f2f2f8f6f2cfc9d4298afef7ac99f04
SayanIndra83 <sayanindra0@gmail.com>
2026-07-13 02:30:09 +0530

    frontend template added

---
+++ src/data/navlinks.ts
@@ -0,0 +1,8 @@
+import { INavLink } from "@/dataTypes";
+
+export const navlinks: INavLink[] = [
+    { name: "Home", href: "/" },
+    { name: "Features", href: "#features" },
+    { name: "Testimonials", href: "#testimonials" },
+    { name: "Pricing", href: "#pricing" },
+];
